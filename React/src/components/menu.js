c89a340283a9bb04e630b4ae60e2997c18690caa
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-05-26 20:06:24 +0530

    Add on click discription

--- React/src/components/menu.js
+++ React/src/components/menu.js
@@ -1,12 +1,42 @@
 import React from "react";
-import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+import { Card, CardImg, CardImgOverlay, CardTitle, CardBody } from "reactstrap";
 
 class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedDish: null,
+    };
+  }
+  handleClick = (dish) => {
+    if (this.state.selectedDish !== dish) {
+      this.setState({ selectedDish: dish });
+    } else {
+      this.setState({ selectedDish: null });
+    }
+  };
+
+  renderDish(dish) {
+    if (dish) {
+      return (
+        <Card>
+          <CardImg width="100%" src={dish.image} alt={dish.name} />
+          <CardImgOverlay>
+            <CardTitle heading="true">{dish.name}</CardTitle>
+          </CardImgOverlay>
+          <CardBody>{dish.description}</CardBody>
+        </Card>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+
   render() {
     const menu = this.props.dishes.map((dish) => {
       return (
         <div key={dish.id} className="col-12 col-md-5 m-1 mt-5">
-          <Card>
+          <Card onClick={() => this.handleClick(dish)}>
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
               <CardTitle heading="true">{dish.name}</CardTitle>
@@ -19,6 +49,7 @@ class Menu extends React.Component {
     return (
       <div className="container mt-4">
         <div className="row">{menu}</div>
+        <div className="row">{this.renderDish(this.state.selectedDish)}</div>
       </div>
     );
   }
