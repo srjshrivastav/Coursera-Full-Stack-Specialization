2015eb0b3e52e36a9cf9e3ce3cbd9e5b826de1ae
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-05-26 23:05:09 +0530

    Showing comments of a dish

--- React/src/components/menu.js
+++ React/src/components/menu.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Card, CardImg, CardImgOverlay, CardTitle, CardBody } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardTitle,
+  CardBody,
+  CardText,
+} from "reactstrap";
 
 class Menu extends React.Component {
   constructor(props) {
@@ -16,8 +23,43 @@ class Menu extends React.Component {
     }
   };
 
+  renderDish(dish) {
+    if (dish) {
+      const { comments } = dish;
+      return (
+        <div className="row">
+          <div className="col-12 col-sm-5 m-1">
+            <Card>
+              <CardImg width="100%" src={dish.image} alt={dish.name} />
+              <CardBody>
+                <CardTitle>
+                  <h4>{dish.name}</h4>
+                </CardTitle>
+                <CardText>{dish.description}</CardText>
+              </CardBody>
+            </Card>
+          </div>
+          <div className="col-sm-5 m-1">
+            <h3>Comments</h3>
+            {comments.map((comment) => (
+              <div key={comment.id}>
+                <div>
+                  <p style={{ fontSize: 18 }}>{comment.comment}</p>
+                  <p style={{ fontSize: 18 }}>
+                    --{comment.author}, {new Date(comment.date).toDateString()}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+
   render() {
-    const { selectedDish } = this.state;
     const menu = this.props.dishes.map((dish) => {
       return (
         <div key={dish.id} className="col-12 col-md-5 m-1 mt-5">
@@ -26,15 +68,6 @@ class Menu extends React.Component {
             <CardImgOverlay>
               <CardTitle heading="true">{dish.name}</CardTitle>
             </CardImgOverlay>
-            {selectedDish ? (
-              selectedDish.name === dish.name ? (
-                <CardBody>{dish.description}</CardBody>
-              ) : (
-                <div></div>
-              )
-            ) : (
-              <div></div>
-            )}
           </Card>
         </div>
       );
@@ -43,6 +76,7 @@ class Menu extends React.Component {
     return (
       <div className="container mt-4">
         <div className="row">{menu}</div>
+        <div>{this.renderDish(this.state.selectedDish)}</div>
       </div>
     );
   }
