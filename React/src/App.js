3739aedeff1af617016c0e06e4eb3139aba522dc
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-05-26 19:46:31 +0530

    Separate the dishes array in shared folder

--- React/src/App.js
+++ React/src/App.js
@@ -1,18 +1,27 @@
 import React from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./components/menu";
+import { DISHES } from "./shared/dishes";
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary" fixed="top">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary" fixed="top">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
