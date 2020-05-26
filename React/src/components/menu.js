5f56a1ecd24f66b596fe4470c64d37e588822322
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-05-26 19:53:29 +0530

    remove media and card

--- React/src/components/menu.js
+++ React/src/components/menu.js
@@ -1,29 +1,24 @@
 import React from "react";
-import { Media } from "reactstrap";
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
 class Menu extends React.Component {
   render() {
     const menu = this.props.dishes.map((dish) => {
       return (
-        <div key={dish.id} className="col-12 mt-5">
-          <Media tag="li">
-            <Media left middle>
-              <Media object src={dish.image} alt={dish.name} />
-            </Media>
-            <Media body className="ml-5">
-              <Media heading>{dish.name}</Media>
-              <p>{dish.description}</p>
-            </Media>
-          </Media>
+        <div key={dish.id} className="col-12 col-md-5 m-1 mt-5">
+          <Card>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+              <CardTitle heading="true">{dish.name}</CardTitle>
+            </CardImgOverlay>
+          </Card>
         </div>
       );
     });
 
     return (
       <div className="container mt-4">
-        <div className="row">
-          <Media list>{menu}</Media>
-        </div>
+        <div className="row">{menu}</div>
       </div>
     );
   }
