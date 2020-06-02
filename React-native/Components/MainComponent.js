293ca204f79334f3976a172af430ae8b1f678a7c
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-06-03 02:10:59 +0530

    Navigation part 1

--- React-native/Components/MainComponent.js
+++ React-native/Components/MainComponent.js
@@ -3,6 +3,8 @@ import Menu from "./MenuComponent";
 import { DISHES } from "../shared/dishes";
 import Dishdetail from "./DishdetailComponent";
 import { View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
 class Main extends Component {
   constructor(props) {
@@ -17,12 +19,17 @@ class Main extends Component {
   }
 
   render() {
-    return (
-      <View style={{ flex: 1 }}>
+    const Stack = createStackNavigator();
+    const menu = () => {
+      return (
         <Menu
           dishes={this.state.dishes}
           onPress={(dishId) => this.onDishSelect(dishId)}
         />
+      );
+    };
+    const dishDetail = () => {
+      return (
         <Dishdetail
           dish={
             this.state.dishes.filter(
@@ -30,7 +37,15 @@ class Main extends Component {
             )[0]
           }
         />
-      </View>
+      );
+    };
+    return (
+      <NavigationContainer>
+        <Stack.Navigator style={{ flex: 1 }}>
+          <Stack.Screen name="Menu" component={menu} />
+          <Stack.Screen name="DishDetail" component={dishDetail} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
