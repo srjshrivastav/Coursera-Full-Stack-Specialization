3ad1000364146ad5b921c482d757542080d4ac45
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-06-03 19:33:37 +0530

    Add drawer items and toggle drawer

--- React-native/Components/MainComponent.js
+++ React-native/Components/MainComponent.js
@@ -4,11 +4,23 @@ import { DISHES } from "../shared/dishes";
 import Dishdetail from "./DishdetailComponent";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import {
+  createDrawerNavigator,
+  DrawerItemList,
+} from "@react-navigation/drawer";
 import Home from "./HomeComponent";
 import AboutUs from "./AboutComponent";
 import { LEADERS } from "../shared/leaders";
 import Contact from "./ContactComponent";
+import { Icon } from "react-native-elements";
+import {
+  ScrollView,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Image,
+  Text,
+} from "react-native";
 
 const Stack = createStackNavigator();
 
@@ -26,7 +38,21 @@ function MenuStack(props) {
         },
       }}
     >
-      <Stack.Screen name="Menu">
+      <Stack.Screen
+        name="Menu"
+        options={{
+          headerLeft: () => {
+            return (
+              <Icon
+                onPress={() => props.navigation.toggleDrawer()}
+                title="menu"
+                color="#fff"
+                name="menu"
+              />
+            );
+          },
+        }}
+      >
         {(props) => <Menu {...props} dishes={DISHES} />}
       </Stack.Screen>
       <Stack.Screen name="DishDetail" component={Dishdetail} />
@@ -46,6 +72,16 @@ function HomeStack(props) {
         headerTitleStyle: {
           color: "#fff",
         },
+        headerLeft: () => {
+          return (
+            <Icon
+              onPress={() => props.navigation.toggleDrawer()}
+              title="menu"
+              color="#fff"
+              name="menu"
+            />
+          );
+        },
       }}
     >
       <Stack.Screen name="Home" component={Home} />
@@ -64,6 +100,16 @@ function AboutStack(props) {
         headerTintColor: "#fff",
         headerTitleStyle: {
           color: "#fff",
+        },
+        headerLeft: () => {
+          return (
+            <Icon
+              onPress={() => props.navigation.toggleDrawer()}
+              title="menu"
+              color="#fff"
+              name="menu"
+            />
+          );
         },
       }}
     >
@@ -86,12 +132,43 @@ function ConatctStack(props) {
         headerTitleStyle: {
           color: "#fff",
         },
+        headerLeft: () => {
+          return (
+            <Icon
+              onPress={() => props.navigation.toggleDrawer()}
+              title="menu"
+              color="#fff"
+              name="menu"
+            />
+          );
+        },
       }}
     >
       <Stack.Screen name="Conatct Us" component={Contact} />
     </Stack.Navigator>
   );
 }
+const CustomDrawerContentComponent = (props) => (
+  <ScrollView>
+    <SafeAreaView
+      style={styles.container}
+      forceInset={{ top: "always", horizontal: "never" }}
+    >
+      <View style={styles.drawerHeader}>
+        <View style={{ flex: 1 }}>
+          <Image
+            source={require("./images/logo.png")}
+            style={styles.drawerImage}
+          />
+        </View>
+        <View style={{ flex: 2 }}>
+          <Text style={styles.drawerHeaderText}>Ristorante Con Fusion</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+    <DrawerItemList {...props} />
+  </ScrollView>
+);
 
 class Main extends Component {
   render() {
@@ -104,6 +181,7 @@ class Main extends Component {
             backgroundColor: "#D1C4E9",
           }}
           drawerPosition="left"
+          drawerContent={(props) => <CustomDrawerContentComponent {...props} />}
         >
           <Drawer.Screen name="Home" component={HomeStack} />
           <Drawer.Screen name="About US" component={AboutStack} />
@@ -114,5 +192,29 @@ class Main extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  drawerHeader: {
+    backgroundColor: "#512DA8",
+    height: 140,
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    flexDirection: "row",
+  },
+  drawerHeaderText: {
+    color: "white",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  drawerImage: {
+    margin: 10,
+    width: 80,
+    height: 60,
+  },
+});
 
 export default Main;
