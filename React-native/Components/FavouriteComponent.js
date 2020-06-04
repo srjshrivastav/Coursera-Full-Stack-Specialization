6b3d227cc16e80d01d9b553dc8dbc9402d992493
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-06-04 18:27:07 +0530

    Add delete favourite feature

--- React-native/Components/FavouriteComponent.js
+++ React-native/Components/FavouriteComponent.js
@@ -3,6 +3,8 @@ import { FlatList, View, Text } from "react-native";
 import { ListItem } from "react-native-elements";
 import { connect } from "react-redux";
 import { Loading } from "./LoadingComponent";
+import Swipeout from "react-native-swipeout";
+import { deleteFavorite } from "../redux/ActionCreators";
 import { baseUrl } from "../shared/baseUrl";
 
 const mapStateToProps = (state) => {
@@ -11,6 +13,9 @@ const mapStateToProps = (state) => {
     favorites: state.favorites,
   };
 };
+const mapDispatchToProps = (dispatch) => ({
+  deleteFavorite: (dishId) => dispatch(deleteFavorite(dishId)),
+});
 
 class Favorites extends Component {
   static navigationOptions = {
@@ -19,17 +24,25 @@ class Favorites extends Component {
 
   render() {
     const { navigate } = this.props.navigation;
-
     const renderMenuItem = ({ item, index }) => {
+      const rightButton = [
+        {
+          text: "Delete",
+          type: "delete",
+          onPress: () => this.props.deleteFavorite(item.id),
+        },
+      ];
       return (
-        <ListItem
-          key={index}
-          title={item.name}
-          subtitle={item.description}
-          hideChevron={true}
-          onPress={() => navigate("Dishdetail", { dishId: item.id })}
-          leftAvatar={{ source: { uri: baseUrl + item.image } }}
-        />
+        <Swipeout right={rightButton} autoClose={true}>
+          <ListItem
+            key={index}
+            title={item.name}
+            subtitle={item.description}
+            hideChevron={true}
+            onPress={() => navigate("DishDetail", { dishId: item.id })}
+            leftAvatar={{ source: { uri: baseUrl + item.image } }}
+          />
+        </Swipeout>
       );
     };
 
@@ -55,4 +68,4 @@ class Favorites extends Component {
   }
 }
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
--- React-native/redux/ActionCreators.js
+++ React-native/redux/ActionCreators.js
@@ -183,3 +183,7 @@ export const addComment = (comment) => ({
   type: ActionTypes.ADD_COMMENT,
   payload: comment,
 });
+export const deleteFavorite = (dishId) => ({
+  type: ActionTypes.DELETE_FAVORITE,
+  payload: dishId,
+});
