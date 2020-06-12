249ccf212a8ab1bc07e20b2cdb45aec8ba9cddb6
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-06-12 22:04:14 +0530

    Express Generator REST API

---
+++ Nodejs/conFusionServer/routes/leaderRouter.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const leaderRouter = express.Router();
+
+leaderRouter.use(bodyParser.json());
+
+leaderRouter
+  .route("/")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((req, res, next) => {
+    res.end("Will send all the leaders to you!");
+  })
+  .post((req, res, next) => {
+    res.end(
+      "Will add the leader: " +
+        req.body.name +
+        " with details: " +
+        req.body.description
+    );
+  })
+  .put((req, res, next) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /leaders");
+  })
+  .delete((req, res, next) => {
+    res.end("Deleting all leaders");
+  });
+
+leaderRouter
+  .route("/:leaderId")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((req, res, next) => {
+    res.end(`Will send the leader ${req.params.leaderId} to you!`);
+  })
+  .post((req, res, next) => {
+    res.end(
+      `Will add the leader:${req.params.leaderId} ` +
+        req.body.name +
+        " with details: " +
+        req.body.description
+    );
+  })
+  .put((req, res, next) => {
+    res.statusCode = 403;
+    res.end(`PUT operation not supported on /leaders/${req.params.leaderId}`);
+  })
+  .delete((req, res, next) => {
+    res.end(`will delete leader ${req.params.leaderId} `);
+  });
+
+module.exports = leaderRouter;
