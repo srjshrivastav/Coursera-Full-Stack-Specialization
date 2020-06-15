18a7b88c1eaf07b0ba3829b78410093de9b95f47
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-06-15 23:34:17 +0530

    created a comment scheme and pass it to dish schema

--- Nodejs/node-mongoose/index.js
+++ Nodejs/node-mongoose/index.js
@@ -3,22 +3,44 @@ const mongoose = require("mongoose");
 const Dishes = require("./models/dishes");
 
 const url = "mongodb://localhost:27017/conFusion";
-const connect = mongoose.connect(url);
+const connect = mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 connect.then((db) => {
   console.log("Connected correctly to server");
 
   Dishes.create({
-    name: "Uthapizza",
-    description: "Test",
+    name: "Uthappizza",
+    description: "test",
   })
     .then((dish) => {
       console.log(dish);
 
-      return Dishes.find({}).exec();
+      return Dishes.findByIdAndUpdate(
+        dish._id,
+        {
+          $set: { description: "Updated test" },
+        },
+        {
+          new: true,
+        }
+      ).exec();
     })
-    .then((dishes) => {
-      console.log(dishes);
+    .then((dish) => {
+      console.log(dish);
+
+      dish.comments.push({
+        rating: 5,
+        comment: "I'm getting a sinking feeling!",
+        author: "Leonardo di Carpaccio",
+      });
+
+      return dish.save();
+    })
+    .then((dish) => {
+      console.log(dish);
 
       return Dishes.deleteMany({});
     })
