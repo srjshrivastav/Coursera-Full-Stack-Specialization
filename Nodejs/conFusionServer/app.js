7acc164279062b96b185d4ee86e1f033dfe5979e
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-06-21 19:43:36 +0530

    Basic Authentication

--- Nodejs/conFusionServer/app.js
+++ Nodejs/conFusionServer/app.js
@@ -11,8 +11,6 @@ var leadersRouter = require("./routes/leaderRouter");
 var promoRouter = require("./routes/promotionRouter");
 const mongoose = require("mongoose");
 
-const Dishes = require("./models/dishes");
-
 const url = "mongodb://localhost:27017/conFusion";
 const connect = mongoose.connect(url);
 
@@ -30,6 +28,31 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
+function auth(req, res, next) {
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+    var err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    next(err);
+    return;
+  }
+  var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+    .toString()
+    .split(":");
+  var user = auth[0];
+  var pass = auth[1];
+  if (user == "admin" && pass == "password") {
+    next(); // authorized
+  } else {
+    var err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    next(err);
+  }
+}
+
+app.use(auth);
 
 app.use(logger("dev"));
 app.use(express.json());
