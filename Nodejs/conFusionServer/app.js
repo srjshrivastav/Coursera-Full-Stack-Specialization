3ac24198f8c2daeea45cc87537ff7e4ab39a1b6c
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-07-22 01:53:02 +0530

    express session and create a user model and signup and login and logout working

--- Nodejs/conFusionServer/app.js
+++ Nodejs/conFusionServer/app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var session = require("express-session");
+var fileStore = require("session-file-store")(session);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -34,37 +36,27 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser("12345-67890-09876-54321"));
+// app.use(cookieParser("12345-67890-09876-54321"));
+app.use(
+  session({
+    name: "session-id",
+    secret: "12345-67890-09876-54321",
+    saveUninitialized: false,
+    resave: false,
+    store: new fileStore(),
+  })
+);
 
 function auth(req, res, next) {
-  // console.log("Signed cokkies", req.signedCookies);
-  if (!req.signedCookies.user) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      var err = new Error("You are not authorized");
+  // console.log("Session here", req.session);
+  if (!req.session.user) {
+    var err = new Error("You are not authorized");
 
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
-
-    var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
-      .toString()
-      .split(":");
-    var username = auth[0];
-    var password = auth[1];
-    if (username === "admin" && password === "pass") {
-      res.cookie("user", "admin", { signed: true });
-      next();
-    } else {
-      var err = new Error("You are not authorized");
-
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      return next(err);
-    }
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    return next(err);
   } else {
-    if (req.signedCookies.user === "admin") {
+    if (req.session.user === "authenticated") {
       next();
     } else {
       var err = new Error("You are not authorized");
@@ -75,13 +67,11 @@ function auth(req, res, next) {
     }
   }
 }
-
-app.use(auth);
-
 app.use(express.static(path.join(__dirname, "public")));
-
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use(auth);
+
 app.use("/dishes", dishRouter);
 app.use("/leaders", leadersRouter);
 app.use("/promotions", promoRouter);
