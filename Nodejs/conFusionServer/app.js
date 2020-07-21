0f15c85a3aed22f3aed88486057e2f2b70adcb34
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-07-22 00:06:03 +0530

    created cookies in this commit

--- Nodejs/conFusionServer/app.js
+++ Nodejs/conFusionServer/app.js
@@ -34,31 +34,45 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser("12345-67890-09876-54321"));
+
 function auth(req, res, next) {
-  console.log(req.headers);
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    var err = new Error("You are not authorized");
+  // console.log("Signed cokkies", req.signedCookies);
+  if (!req.signedCookies.user) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      var err = new Error("You are not authorized");
 
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    return next(err);
-  }
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
 
-  var auth = new Buffer(authHeader.split(" ")[1], "base64")
-    .toString()
-    .split(":");
-  var username = auth[0];
-  var password = auth[1];
-  if (username === "admin" && password === "pass") {
-    next();
+    var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+      .toString()
+      .split(":");
+    var username = auth[0];
+    var password = auth[1];
+    if (username === "admin" && password === "pass") {
+      res.cookie("user", "admin", { signed: true });
+      next();
+    } else {
+      var err = new Error("You are not authorized");
+
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
   } else {
-    var err = new Error("You are not authorized");
+    if (req.signedCookies.user === "admin") {
+      next();
+    } else {
+      var err = new Error("You are not authorized");
 
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    return next(err);
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
   }
 }
 
