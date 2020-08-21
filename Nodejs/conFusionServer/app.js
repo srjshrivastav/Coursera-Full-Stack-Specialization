5d0a605e227522e2d2e06eded5939ee19702bacd
Suraj Shrivastava <srjshrivastav1@gmail.com>
2020-08-22 02:38:56 +0530

    Complete nodejs course

--- Nodejs/conFusionServer/app.js
+++ Nodejs/conFusionServer/app.js
@@ -53,6 +53,7 @@ app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser("12345-67890-09876-54321"));
 
 app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
